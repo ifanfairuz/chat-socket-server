1671ca23b3940b35354881650e3ce0f1adf9b761
Ifan Fairuz <ifanfairuz@gmail.com>
2020-10-19 23:42:45 +0700

    [ADD] Documentation

--- src/network/response.ts
+++ src/network/response.ts
@@ -1,8 +1,20 @@
+/**
+ * @description base response format
+ * @interface Response @extends Object
+ * @property {boolean} status boolean
+ * @property {string} message string
+ */
 interface Response extends Object {
   status: boolean
   message: string
 }
 
+/**
+ * @description base Error interface
+ * @interface Error @extends Object
+ * @property {string} type string
+ * @property {any} error any
+ */
 export interface Error extends Object {
   type: string
   error: any

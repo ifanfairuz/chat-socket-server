9316b4ece90c855c4ab99cbf42c23992a21b220d
Ifan Fairuz <ifanfairuz@gmail.com>
2020-10-19 17:06:06 +0700

    [ADD] load all repository

--- src/repository/index.ts
+++ src/repository/index.ts
@@ -1,5 +1,9 @@
+import ChatRepository from './chat/repository'
+import SessionRepository from './session/repository'
 import TokenRepository from './token/repository'
 
 export {
+  ChatRepository,
+  SessionRepository,
   TokenRepository
 }
